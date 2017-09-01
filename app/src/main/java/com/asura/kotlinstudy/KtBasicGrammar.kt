7fe488b9277813680f5d1455606c74e83fce0b87
Asura <709239339@qq.com>
2017-09-01 17:43:29 +0800

    Initial commit

---
+++ app/src/main/java/com/asura/kotlinstudy/KtBasicGrammar.kt
@@ -0,0 +1,32 @@
+package com.asura.kotlinstudy
+
+import android.util.Log
+
+/**
+ * Created by Liuxd on 2017/9/1 15:07.
+ * Kotlin 基础语法
+ * http://www.runoob.com/kotlin/kotlin-basic-syntax.html
+ */
+class KtBasicGrammar {
+    private val TAG = "Asura";
+    fun main(args: Array<String>) {
+        Log.d(TAG, sum(1, 1).toString())
+        Log.d(TAG, sum1(1, 1).toString())
+        Log.d(TAG, sum2(2, 2).toString())
+        Log.d(TAG, sum4(4, 4).toString())
+    }
+
+    //    函数定义使用关键字 fun，参数格式为：参数 : 类型
+    fun sum(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    //    表达式作为函数体，返回类型自动推断：
+    fun sum1(a: Int, b: Int) = a + b
+
+    public fun sum2(a: Int, b: Int): Int = a + b // public 方法则必须明确写出返回类型
+    //    无返回值的函数(类似Java中的void)：
+    fun sum4(a: Int, b: Int): Unit {
+        a + b;
+    }
+}
---
+++ app/src/main/java/com/asura/kotlinstudy/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.asura.kotlinstudy
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private val TAG: String? = "Asura";
+    private var mRv_ktSkill: RecyclerView? = null;
+    private var mLayoutManager: LinearLayoutManager? = null;
+    private var mAdapter: MyAdapter? = null;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRv_ktSkill = findViewById(R.id.rv_ktSkill) as RecyclerView;
+        initRv();
+    }
+
+    private fun initRv() {
+        mLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mAdapter = MyAdapter(resources.getStringArray(R.array.skill_names)
+                , MyAdapter.CallBack { itemView, position ->
+            Toast.makeText(this, "" + position, Toast.LENGTH_SHORT).show();
+            doSomeThing(position)
+        })
+        mRv_ktSkill!!.layoutManager = mLayoutManager;
+        mRv_ktSkill!!.adapter = mAdapter;
+    }
+
+    private fun doSomeThing(position: Int) {
+        if (position == 0) {
+            helloWorld()
+        } else if (position == 1) {
+            val ktBasicGrammar: KtBasicGrammar = KtBasicGrammar()
+            val empty = emptyArray<String>();
+            ktBasicGrammar.main(empty);
+        }
+    }
+
+    private fun helloWorld() {
+        Log.d(TAG, "Hello World");
+    }
+}
