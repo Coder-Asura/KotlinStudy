6a6ae089bf6434417bc91743dccd020762fd3acc
Asura <qq709238339@users.noreply.github.com>
2019-11-17 22:38:11 +0800

    update gradle version

--- app/src/main/java/com/asura/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/asura/kotlinstudy/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.asura.kotlinstudy
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var mRv_ktSkill: RecyclerView? = null
     private var mLayoutManager: LinearLayoutManager? = null
     private var mAdapter: MyAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mRv_ktSkill = findViewById(R.id.rv_ktSkill) as RecyclerView
         initRv()
     }
 
@@ -21,42 +22,31 @@ class MainActivity : AppCompatActivity() {
         mLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         mAdapter = MyAdapter(resources.getStringArray(R.array.skill_names)
                 , MyAdapter.CallBack { itemView, position ->
-            Toast.makeText(this, "" + position, Toast.LENGTH_SHORT).show()
+            toast("$position--->${(itemView as TextView).text}")
             doSomeThing(position)
         })
-        mRv_ktSkill!!.layoutManager = mLayoutManager
-        mRv_ktSkill!!.adapter = mAdapter
+        rv_ktSkill.layoutManager = mLayoutManager
+        rv_ktSkill.adapter = mAdapter
     }
 
     private fun doSomeThing(position: Int) {
-        if (position == 0) {
-            say("World")
-        } else if (position == 1) {
-            val ktBasicGrammar: KtBasicGrammar = KtBasicGrammar()
-            val empty = emptyArray<String>()
-            ktBasicGrammar.main(empty)
-        } else if (position == 2) {
-            val ktBasicDataType: KtBasicDataType = KtBasicDataType()
-            val empty = emptyArray<String>()
-            ktBasicDataType.main(empty)
-        } else if (position == 3) {
-            val ktConditionControl: KtConditionControl = KtConditionControl()
-            val empty = emptyArray<String>()
-            ktConditionControl.main(empty)
-        } else if (position == 4) {
-            val ktLoopControl: KtLoopControl = KtLoopControl()
-            val empty = emptyArray<String>()
-            ktLoopControl.main(empty)
-        } else if (position == 5) {
-            KtClassAndObject().main()
-        } else if (position == 6) {
-            KtExtends().main()
-        } else if (position == 7) {
-            KtInterface().main()
+        val positions = resources.getIntArray(R.array.skill_position)
+        when (position) {
+            positions[0] -> say("World")
+            positions[1] -> KtBasicGrammar().main(emptyArray())
+            positions[2] -> KtBasicDataType().main(emptyArray())
+            positions[3] -> KtConditionControl().main(emptyArray())
+            positions[4] -> KtLoopControl().main(emptyArray())
+            positions[5] -> KtClassAndObject().main()
+            positions[6] -> KtExtends().main()
+            positions[7] -> KtInterface().main()
         }
     }
 
     private fun say(msg: String) {
         ALog.d("Hello $msg")
     }
+
+    //扩展函数
+    private fun Activity.toast(msg: String) = run { Toast.makeText(this, msg, Toast.LENGTH_SHORT).show() }
 }
